ba364f6a59a73e84e087105844ad13c393750ddf
BradleyGilden <bradleygilden@gmail.com>
2023-12-27 22:15:31 +0200

    hero section

--- Landing_Page/tailwind.config.js
+++ Landing_Page/tailwind.config.js
@@ -2,18 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      'fg': '#f2f2f2',
-      'bg': '#04040b',
-      'primary': '#f0c000',
-      'secondary': '#b8bbb9',
-      'accent': '#b01719'
+    extend: {
+      colors: {
+        'fg': '#f2f2f2',
+        'bg': '#04040b',
+        'primary': '#f0c000',
+        'secondary': '#b8bbb9',
+        'accent': '#b01719'
+      },
+      textColor : {
+        'fg': '#f2f2f2',
+        'bg': '#04040b',
+        'primary': '#f0c000',
+        'secondary': '#b8bbb9',
+        'accent': '#b01719'
+      }
     },
     fontFamily: {
-      'header': [],
-      'body': []
+      'header': ['"Montserrat Alternates"', 'sans-serif'],
+      'body': ['Inter', 'sans-serif']
+    },
+    backgroundImage: {
+      'hero-img': "url('../images/gymlayout.jpg')",
     }
+
   },
   plugins: [],
 }
