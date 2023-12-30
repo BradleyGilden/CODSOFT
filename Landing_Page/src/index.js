8914136ded342011c18bb58d20f00262875c20e6
BradleyGilden <bradleygilden@gmail.com>
2023-12-30 13:08:24 +0200

    prettier formatted

--- Landing_Page/src/index.js
+++ Landing_Page/src/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const socials = document.querySelectorAll('.socials');
-  const navItems = document.querySelectorAll('.nav-menu');
-  const menu = document.getElementById('menu');
-  const menuList = document.getElementById('menu-list');
+  const socials = document.querySelectorAll(".socials");
+  const navItems = document.querySelectorAll(".nav-menu");
+  const menu = document.getElementById("menu");
+  const menuList = document.getElementById("menu-list");
   var elem = document.querySelector(".main-carousel");
   var flkty = new Flickity(elem, {
     // options
@@ -18,36 +18,37 @@ document.addEventListener("DOMContentLoaded", () => {
     friction: 0.2,
   });
 
-
   for (let social of socials) {
-    social.addEventListener('mouseover', () => {
+    social.addEventListener("mouseover", () => {
       let source = social.src;
       source = source.replace("b8bbb9", "f0c000");
       social.src = source;
-    })
-    social.addEventListener('mouseout', () => {
+    });
+    social.addEventListener("mouseout", () => {
       let source = social.src;
       source = source.replace("f0c000", "b8bbb9");
       social.src = source;
-    })
+    });
   }
 
   for (let item of navItems) {
-    item.addEventListener('click', () => {
-      console.log(item.dataset.id)
-      gsap.to(window, { duration: 2, scrollTo: item.dataset.id, ease: 'power2' });
-    })
+    item.addEventListener("click", () => {
+      console.log(item.dataset.id);
+      gsap.to(window, {
+        duration: 2,
+        scrollTo: item.dataset.id,
+        ease: "power2",
+      });
+    });
   }
 
-  menu.addEventListener('click', () => {
-    if (menuList.classList.contains('hidden')) {
-      menuList.classList.remove('hidden');
-      menuList.classList.add('flex');
+  menu.addEventListener("click", () => {
+    if (menuList.classList.contains("hidden")) {
+      menuList.classList.remove("hidden");
+      menuList.classList.add("flex");
+    } else if (menuList.classList.contains("flex")) {
+      menuList.classList.remove("flex");
+      menuList.classList.add("hidden");
     }
-    else if (menuList.classList.contains('flex')) {
-      menuList.classList.remove('flex');
-      menuList.classList.add('hidden');
-    }
-  })
-
+  });
 });
