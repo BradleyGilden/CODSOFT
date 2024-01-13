8ec65725ad796061e030cba756be820bd75dba07
BradleyGilden <bradleygilden@gmail.com>
2024-01-13 19:24:57 +0200

    prettier doc format

--- Level_1/Calculator/src/index.js
+++ Level_1/Calculator/src/index.js
@@ -1,50 +1,50 @@
-/** 
+/**
  * This script will controll all the calculator log for the web calculator
-*/
+ */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // equation input for calc screen
   let clist = [];
-  const operators = ['/', '*', '+', '-'];
+  const operators = ["/", "*", "+", "-"];
   let equated = false;
   let equation = [];
   let first = true;
   let mtext = [];
   let current;
   let value;
-  const calcScreen = document.querySelector('#c-screen');
-  const mainScreen = document.querySelector('#m-screen');
+  const calcScreen = document.querySelector("#c-screen");
+  const mainScreen = document.querySelector("#m-screen");
 
   // clearing the screens
-  document.querySelector('#clear').addEventListener('click', () => {
+  document.querySelector("#clear").addEventListener("click", () => {
     clist = [];
     mtext = [];
     equation = [];
-    mainScreen.textContent = '';
+    mainScreen.textContent = "";
   });
 
-  document.querySelector('#numpad').addEventListener('click', (e) => {
+  document.querySelector("#numpad").addEventListener("click", (e) => {
     value = e.target.dataset.value;
 
     // remove current error
-    if (mainScreen.textContent === 'Invalid Input!!!') {
-      mainScreen.textContent = '';
-      mainScreen.style.color = '#f1f1f1';
+    if (mainScreen.textContent === "Invalid Input!!!") {
+      mainScreen.textContent = "";
+      mainScreen.style.color = "#f1f1f1";
     }
 
     // clear default content
     if (first) {
-      calcScreen.textContent = '';
-      mainScreen.textContent = '';
+      calcScreen.textContent = "";
+      mainScreen.textContent = "";
       first = false;
     }
     // append items in calcscreen
-    if (value !== '=') {
-      equated ? clist = [value]: clist.push(value);
-      calcScreen.textContent = clist.join('');
+    if (value !== "=") {
+      equated ? (clist = [value]) : clist.push(value);
+      calcScreen.textContent = clist.join("");
     }
 
-    if (!isNaN(value) || value === '.') {
+    if (!isNaN(value) || value === ".") {
       // if a person has already pressed the equal to sign, then goes for a number,
       // break the chan and display that number only
       if (equated) {
@@ -52,29 +52,30 @@ document.addEventListener('DOMContentLoaded', function() {
       } else {
         mtext.push(value);
       }
-      mainScreen.textContent = mtext.join('');
+      mainScreen.textContent = mtext.join("");
       equated = false;
     }
-    
+
     if (operators.includes(value)) {
-      console.log('from operators', equation);
+      console.log("from operators", equation);
       current = mainScreen.textContent;
 
       // clear main screen content
-      mainScreen.textContent = '';
+      mainScreen.textContent = "";
       mtext = [];
 
       equation.push(current, value);
-      console.log('from operators', equation);
-    } else if (value === '=') {
+      console.log("from operators", equation);
+    } else if (value === "=") {
       let result;
       current = mainScreen.textContent;
-      if (operators.includes(equation[equation.length - 1])) equation.push(current);
+      if (operators.includes(equation[equation.length - 1]))
+        equation.push(current);
       result = getResult(equation);
       // in case of an error
-      if (result === '' && equation.length !== 0) {
-        mainScreen.textContent = 'Invalid Input!!!';
-        mainScreen.style.color = 'red';
+      if (result === "" && equation.length !== 0) {
+        mainScreen.textContent = "Invalid Input!!!";
+        mainScreen.style.color = "red";
       } else {
         mainScreen.textContent = result.toString();
       }
@@ -83,25 +84,23 @@ document.addEventListener('DOMContentLoaded', function() {
       calcScreen.textContent = result;
       equated = true;
     }
-  })
-})
-
-
+  });
+});
 
 /**
  * getResult - gets the final calculation result using many processing functions
- * @param {list} calc_list 
+ * @param {list} calc_list
  * @returns and int | float result
  */
 function getResult(calc_list) {
-  let calculation = calc_list.filter(item => item !== '');
+  let calculation = calc_list.filter((item) => item !== "");
   calculation = cleanup(calculation);
   calculation = filterOperations(calculation);
   calculation = bodmas(calculation);
   return calculation;
 }
 
-/* 
+/*
  * cleanup - cleans up equation by converting the strings to ints and floats and
  * removes excess operators at the ends
  * Returns: list of int/float operands with trailing operations removed
@@ -109,7 +108,7 @@ function getResult(calc_list) {
 function cleanup(calculation) {
   let calc = calculation;
   let opCount = 0;
-  const ignore = ['*', '+', '/', '-'];
+  const ignore = ["*", "+", "/", "-"];
   try {
     for (let i = 0; i < calc.length; i++) {
       // keep track on number of operations encountered so we can remove them later if theres excess
@@ -119,35 +118,38 @@ function cleanup(calculation) {
         opCount = 0;
       }
       // if a number is preceeded by a '-', make it negative
-      if (calc[i] === '-' && !isNaN(calc[i + 1]) && isNaN(calc[i - 1])) {
-        if (calc[i + 1].includes('.')) {
+      if (calc[i] === "-" && !isNaN(calc[i + 1]) && isNaN(calc[i - 1])) {
+        if (calc[i + 1].includes(".")) {
           calc.splice(i, 2, parseFloat(`-${calc[i + 1]}`));
         } else {
           calc.splice(i, 2, parseInt(`-${calc[i + 1]}`));
         }
         opCount = 0;
-      } else if ((ignore.includes(calc[i])) && i === 0 ) { // removes excess operators at start
+      } else if (ignore.includes(calc[i]) && i === 0) {
+        // removes excess operators at start
         calc.splice(i, 1);
         // go back to first index and try again
         i--;
-      } else if ((ignore.includes(calc[i])) && i === calc.length - 1 ) { // removes excess operators at the end
+      } else if (ignore.includes(calc[i]) && i === calc.length - 1) {
+        // removes excess operators at the end
         calc.splice(i - opCount + 1, opCount);
         break;
       } else if (!isNaN(calc[i])) {
-        if (calc[i].includes('.')) {
+        if (calc[i].includes(".")) {
           calc[i] = parseFloat(calc[i]);
         } else {
           calc[i] = parseInt(calc[i]);
         }
-      } else if (isNaN(calc[i]) && !ignore.includes(calc[i])) { // handle invalid inputs
-        throw new Error('Incorrect type');
+      } else if (isNaN(calc[i]) && !ignore.includes(calc[i])) {
+        // handle invalid inputs
+        throw new Error("Incorrect type");
       }
     }
   } catch (err) {
     console.log(err.message);
     calc = [];
   } finally {
-    return calc
+    return calc;
   }
 }
 
@@ -161,13 +163,16 @@ function filterOperations(calculation) {
   let opCount = 0;
   let highest = null;
   let calc = calculation;
-  const operators = ['-', '+', '*', '/'];
+  const operators = ["-", "+", "*", "/"];
 
   for (let i = 0; i < calc.length; i++) {
     if (operators.includes(calc[i])) {
       opCount++;
       // find operator of highest precidence for current consecutive sequence of operators
-      highest = operators.indexOf(calc[i]) > operators.indexOf(highest) ? calc[i]: highest;
+      highest =
+        operators.indexOf(calc[i]) > operators.indexOf(highest)
+          ? calc[i]
+          : highest;
     } else {
       opCount = 0;
       highest = null;
@@ -190,33 +195,33 @@ function filterOperations(calculation) {
  */
 function calculate(n1, operation, n2) {
   switch (operation) {
-    case '-':
+    case "-":
       return n1 - n2;
-    case '+':
+    case "+":
       return n1 + n2;
-    case '*':
+    case "*":
       return n1 * n2;
-    case '/':
+    case "/":
       return n1 / n2;
-    }
+  }
 }
 
 /**
  * bodmas - performs calculations in order of basic bodmas operations
  */
 function bodmas(calculation) {
-  const operations = ['/', '*', '+', '-']
+  const operations = ["/", "*", "+", "-"];
   let calc = calculation;
 
-  if (calc.length === 0) return '';
+  if (calc.length === 0) return "";
   // in order of bodmas operations
   for (let j of operations) {
     for (let i = 0; i < calc.length && calc.includes(j); i++) {
       // if current index contains current operation
       if (calc[i] === j) {
-        calc.splice(i - 1, 3, calculate(calc[i - 1], calc[i], calc[i + 1]))
+        calc.splice(i - 1, 3, calculate(calc[i - 1], calc[i], calc[i + 1]));
         // iterate back as to not miss the next operation
-        i --;
+        i--;
       }
     }
   }
